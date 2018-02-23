bcd0a67738c2d1a623f75a41256e9c22fcd61087
jackdyett@gmail.com Marston <jackdyett@gmail.com>
2018-02-23 00:39:17 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0
 function init() {
   const body = document.querySelector('body')
-}
+
 
 body.addEventListener('keydown', function(e) {
   const key = parseInt(e.detail || e.which);
@@ -16,3 +16,4 @@ body.addEventListener('keydown', function(e) {
     index = 0;
   }
 })
+}
