a6cced23e61fda6fb8bcc4362a53ffa391d56be2
jackdyett@gmail.com Marston <jackdyett@gmail.com>
2018-02-23 00:09:20 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,4 +4,6 @@ function init() {
   const body = document.querySelector('body')
 }
 
-body.addEventListener('keydown', function(e))
+body.addEventListener('keydown', function(e)) {
+  alert('YEEEEEEHAWWWWWW!!!')
+}
